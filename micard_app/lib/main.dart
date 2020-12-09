eb1210fd977f688846b3672f2d27aa04c4a19f89
Sufyan Arif <72404837+Sufyanarif001@users.noreply.github.com>
2020-12-09 14:21:52 +0500

    create statless widget

--- micard_app/lib/main.dart
+++ micard_app/lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.indigoAccent,
-        body: Container(),
-      )
-    )
+    MyApp()
   );
+}
+
+class MyApp  extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.amber,
+          body: Container(),
+        )
+    );
+  }
 }
